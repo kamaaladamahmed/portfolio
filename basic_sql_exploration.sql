7ce5211f68f69addf64cf435510efb960163f441
kamaaladamahmed <110216084+kamaaladamahmed@users.noreply.github.com>
2022-08-10 22:23:48 -0700

    Update and rename test.sql to basic_sql_exploration.sql

--- basic_sql_exploration.sql
+++ basic_sql_exploration.sql
@@ -1,13 +1,13 @@
--- What are the total numbers in the United States?\
+-- What are the total numbers in the United States?
 
 SELECT 
 	MAX(population) AS population,
 	MAX(total_cases) AS total_cases,
   	MAX(total_deaths) AS total_deaths
 FROM covid_deaths
-WHERE location = 'United States' AND continent IS NOT NULL\
+WHERE location = 'United States' AND continent IS NOT NULL
     
--- Let's break it down by year, and look at both the infection and death rate of each\
+-- Let's break it down by year, and look at both the infection and death rate of each
 
 SELECT 
 	EXTRACT(year FROM date) AS year,
@@ -19,7 +19,7 @@ FROM covid_deaths
 WHERE location = 'United States' AND continent IS NOT NULL
 GROUP BY year
 
--- Let's look at total vaccination data in the United States\
+-- Let's look at total vaccination data in the United States
 
 SELECT
 	MAX(dea.population) AS population,
@@ -32,7 +32,7 @@ JOIN covid_vaccinations vacc
 WHERE
 	dea.location = 'United States' AND dea.continent iS NOT NULL
     
--- Let's see how vaccination rate has grown over time in the United States\
+-- Let's see how vaccination rate has grown over time in the United States
 
 SELECT
 	EXTRACT(year FROM dea.date) AS year,
@@ -47,20 +47,18 @@ WHERE dea.location = 'United States' AND dea.continent iS NOT NULL
 GROUP BY year, month
 ORDER BY year, month
 
--- Now let's compare infection rate, death rate, and vaccination rate every year in the United States\
+-- Now let's compare infection rate, death rate, and vaccination rate every year in the United States
 
 SELECT
-	EXTRACT(year FROM dea.date) AS year,\
-    	ROUND(SUM(new_cases)/MAX(population)*100,3) AS infection_rate,\
-    	ROUND(SUM(new_deaths)/SUM(new_cases)*100,3) AS death_rate,\
-    	ROUND(MAX(vacc.people_fully_vaccinated)/MAX(dea.population)*100,4) AS vaccination_rate\
+	EXTRACT(year FROM dea.date) AS year,
+    	ROUND(SUM(new_cases)/MAX(population)*100,3) AS infection_rate,
+    	ROUND(SUM(new_deaths)/SUM(new_cases)*100,3) AS death_rate,
+    	ROUND(MAX(vacc.people_fully_vaccinated)/MAX(dea.population)*100,4) AS vaccination_rate
 FROM covid_deaths dea
 JOIN covid_vaccinations vacc
 	ON dea.location = vacc.location
     	AND dea.date = vacc.date
 WHERE
 	dea.location = 'United States' AND dea.continent iS NOT NULL
-GROUP BY
-	year
-ORDER BY 
-	year
+GROUP BY year
+ORDER BY year
